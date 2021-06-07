02847c9bd3b94f3cdc4de32bf04efc0c9f593be7
Juancho <juanalvarez9591@gmail.com>
2021-06-07 20:10:31 -0300

    I love react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const hamburger =
+  "https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/165384.jpg";
 
 function App() {
+  const Order = () => console.log("Encargado");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Card style={{ width: "12rem" }}>
+        <Card.Img variant="top" src={hamburger} />
+        <Card.Body>
+          <Card.Title style={{ fontSize: "1.3rem" }}>
+            Hamburgesa con doble queso
+          </Card.Title>
+          <Card.Text style={{ fontSize: "0.7rem" }}>
+            Con mayonesa, ketchup y doble queso cheddar. Incluye papas fritas!
+          </Card.Text>
+          <Button variant="primary" onClick={Order}>
+            Encargar ya!
+          </Button>
+        </Card.Body>
+      </Card>
+    </>
   );
 }
 
