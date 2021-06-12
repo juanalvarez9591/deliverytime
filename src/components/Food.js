98eb8dd161bbd0e10ed2371eb7c6f02128a5afa8
Juan <juanalvarez9591@gmail.com>
2021-06-12 02:37:17 -0300

    Added custom .sass to modify react-bootstrap stuff

--- src/components/Food.js
+++ src/components/Food.js
@@ -2,25 +2,31 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
 function Food(props) {
-  const Order = () => console.log("Encargado");
+	const Order = () => console.log("Encargado");
 
-  return (
-    <>
-      <Card style={{ width: "12rem" }}>
-        <Card.Img variant="top" src={props.url} style={{ height: "8rem" }} />
-        <Card.Body>
-          <Card.Title style={{ fontSize: "1.3rem" }}>{props.title}</Card.Title>
-          <Card.Subtitle style={{ fontSize: "0.7rem" }}>
-            {props.price}
-          </Card.Subtitle>
-          <Card.Text style={{ fontSize: "0.7rem" }}>{props.subtitle}</Card.Text>
-          <Button variant="danger" onClick={Order}>
-            Encargar ya!
-          </Button>
-        </Card.Body>
-      </Card>
-    </>
-  );
+	return (
+		<>
+			<Card style={{ width: "12rem" }}>
+				<Card.Img
+					variant="top"
+					src={props.url}
+					style={{ height: "8rem" }}
+				/>
+				<Card.Body>
+					<Card.Title style={{ fontSize: "1.3rem" }}>
+						{props.title}
+					</Card.Title>
+					<Card.Subtitle style={{ fontSize: "0.7rem" }}>
+						{props.price}
+					</Card.Subtitle>
+					<Card.Text style={{ fontSize: "0.7rem" }}>
+						{props.subtitle}
+					</Card.Text>
+					<Button onClick={Order}>Encargar ya!</Button>
+				</Card.Body>
+			</Card>
+		</>
+	);
 }
 
 export default Food;
