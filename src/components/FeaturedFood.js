8f9acce34b4821c497b5e432ea045db32fa95139
Juan <juanalvarez9591@gmail.com>
2021-06-14 19:35:34 -0300

    Odio css

--- src/components/FeaturedFood.js
+++ src/components/FeaturedFood.js
@@ -1,5 +1,33 @@
+import Carousel from "react-bootstrap/Carousel";
+
 function Featured() {
-	return <p>Featured</p>;
+	function CarouselSize() {
+		if (window.innerWidth > 480) {
+			return { borderRadius: "1rem", width: "40rem" };
+		} else {
+			return { borderRadius: "1rem", width: "20rem" };
+		}
+	}
+
+	return (
+		<div
+			style={{
+				display: "grid",
+				justifyContent: "center",
+				padding: "1rem",
+			}}
+		>
+			<Carousel>
+				<Carousel.Item>
+					<img
+						src="https://upload.wikimedia.org/wikipedia/commons/8/88/McDonald%27s_McDouble_close.jpg"
+						alt="First slide"
+						style={CarouselSize()}
+					/>
+				</Carousel.Item>
+			</Carousel>
+		</div>
+	);
 }
 
 export default Featured;
