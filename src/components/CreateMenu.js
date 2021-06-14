85dac4141551c890bc2d55e7e178d8416f01af88
Juan <juanalvarez9591@gmail.com>
2021-06-14 15:18:13 -0300

    New structure for adding a carrousel

---
+++ src/components/CreateMenu.js
@@ -0,0 +1,68 @@
+import Tab from "react-bootstrap/Tab";
+import Nav from "react-bootstrap/Nav";
+import Food from "./Food";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import data from "./menu.json";
+import React, { useState } from "react";
+
+function CreateMenu(props) {
+	const [menu, changeMenu] = useState(
+		generateComponents(Object.keys(data)[0])
+	);
+
+	function generateComponents(i) {
+		return data[i].map((i) => (
+			<Col key={i.key} style={{ padding: "0.5rem" }}>
+				<Food
+					title={i.title}
+					subtitle={i.subtitle}
+					price={i.price}
+					url={i.url}
+					key={i.key}
+				/>
+			</Col>
+		));
+	}
+
+	function generateTables() {
+		return Object.keys(data).map((i) => (
+			<Nav.Item key={i}>
+				<Nav.Link
+					eventKey={i}
+					onSelect={() => changeMenu(generateComponents(i))}
+					key={i}
+				>
+					{i}
+				</Nav.Link>
+			</Nav.Item>
+		));
+	}
+
+	return (
+		<>
+			<Tab.Container
+				id="left-tabs-example"
+				defaultActiveKey={Object.keys(data)[0]}
+			>
+				<Row style={{ padding: "1rem" }}>
+					<Col sm={3}>
+						<Nav variant="pills" className="flex-column">
+							{generateTables()}
+						</Nav>
+					</Col>
+					<Col sm={9}>
+						<Tab.Content>
+							<Container>
+								<Row style={{ padding: "0.5rem" }}>{menu}</Row>
+							</Container>
+						</Tab.Content>
+					</Col>
+				</Row>
+			</Tab.Container>
+		</>
+	);
+}
+
+export default CreateMenu;
---
+++ src/components/FeaturedFood.js
@@ -0,0 +1,5 @@
+function Featured() {
+	return <p>Featured</p>;
+}
+
+export default Featured;
