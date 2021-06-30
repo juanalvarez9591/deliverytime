ffceed9f1a429f89e5c53d7a8a902c12b5411938
Juan <juanalvarez9591@gmail.com>
2021-06-30 04:16:25 -0300

    added total

--- src/components/CheckoutTable.js
+++ src/components/CheckoutTable.js
@@ -1,22 +1,16 @@
 import { Table } from "react-bootstrap";
 import CheckoutItems from "./CheckoutItems";
+import CheckoutTotal from "./CheckoutTotal";
+
 const CheckoutTable = (props) => {
 	const { titlelist, pricelist } = props;
-
-	const total = (
-		<tr>
-			<td>Total:</td>
-			<td></td>
-			<td>200 REAIS</td>
-		</tr>
-	);
 
 	return (
 		<Table striped responsive id="table">
 			<tbody>
 				<CheckoutItems titlelist={titlelist} pricelist={pricelist} />
 
-				{total}
+				<CheckoutTotal pricelist={pricelist} />
 			</tbody>
 		</Table>
 	);
