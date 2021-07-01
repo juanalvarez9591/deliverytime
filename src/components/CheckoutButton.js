3f48ff6e8d308770b96a311fd9c4a331c5e1097c
Juan <juanalvarez9591@gmail.com>
2021-07-01 17:00:32 -0300

    finished text creation

--- src/components/CheckoutButton.js
+++ src/components/CheckoutButton.js
@@ -6,13 +6,29 @@ const CheckoutButton = (props) => {
 	const checkout = useSelector((state) => state.checkout);
 	const { state } = props;
 
+	const checky = {};
+
+	checkout.titles.forEach(function (x) {
+		checky[x] = (checky[x] || 0) + 1;
+	});
+	console.log(checky);
+
+	const ck = Object.keys(checky);
+	const cv = Object.values(checky);
+
+	const cf = [];
+
+	ck.forEach((i, index) => {
+		cf.push(cv[index] + " " + i);
+	});
+
 	const createOrder = (state) => {
 		const pos = state.pos
 			? " Precisaría el POS para pagar con tarjeta."
 			: "";
 
 		return console.log(
-			`Hola! podrías mandarme: ${checkout.titles.join(", ")} a ${
+			`Hola! podrías mandarme: ${cf.join(", ")} a ${
 				state.address
 			}.${pos} Muchas gracias.`
 		);
