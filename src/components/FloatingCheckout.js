89b3a7a229d8c270f55a4c77be13ab28af2858ae
Juancho <juanalvarez9591@gmail.com>
2021-07-07 14:27:44 -0300

    New feature: floating checkout

---
+++ src/components/FloatingCheckout.js
@@ -0,0 +1,16 @@
+const FloatingCheckout = () => {
+	return (
+		<p
+			style={{
+				position: "fixed",
+				bottom: 0,
+				right: 0,
+				margin: "2rem",
+			}}
+		>
+			Test
+		</p>
+	);
+};
+
+export default FloatingCheckout;
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,12 +1,14 @@
 import CreateMenu from "../components/CreateMenu";
 import FeaturedFood from "../components/FeaturedFood";
+import FloatingCheckout from "../components/FloatingCheckout";
 
 function Menu() {
 	return (
-		<>
+		<div>
 			<FeaturedFood />
 			<CreateMenu />
-		</>
+			<FloatingCheckout />
+		</div>
 	);
 }
 
